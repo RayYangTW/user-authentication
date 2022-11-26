edcd0df67efd8c369f54598593b84944c6b23725
ray199229 <ray199229@gmail.com>
2022-11-26 20:43:58 +0800

    Feat: add check auth function

--- app.js
+++ app.js
@@ -1,14 +1,65 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const app = express()
 const PORT = 3000
 
-app.engine('hbs', exphbs({ detaultLayout: 'main', extname: 'hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', (req, res) => {
+  const { id, password } = req.body
+  const users = [
+ {
+   firstName: 'Tony',
+   email: 'tony@stark.com',
+   password: 'iamironman'
+ },
+ {
+   firstName: 'Steve',
+   email: 'captain@hotmail.com',
+   password: 'icandothisallday'
+ },
+ {
+   firstName: 'Peter',
+   email: 'peter@parker.com',
+   password: 'enajyram'
+ },
+ {
+   firstName: 'Natasha',
+   email: 'natasha@gamil.com',
+   password: '*parol#@$!'
+ },
+ {
+   firstName: 'Nick',
+   email: 'nick@shield.com',
+   password: 'password'
+ }
+]
+  
+  const checkAccount = users.find(user => user.email === id)
+  if(!checkAccount) {
+    res.render('error')
+  } else {
+     const checkPassword = checkAccount.password === password
+    if(!checkPassword) {
+      res.render('error') 
+    } else {
+      const userFirstName = checkAccount.firstName
+      res.render('success', {userFirstName})
+    }
+  }
 })
 
 app.listen(PORT, () => {
